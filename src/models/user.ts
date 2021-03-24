5a1e57505ef89ea62a15e37680b3504ab23239f0
burkaydurdu <burkaydurdu@outlook.com>
2021-03-24 12:21:49 +0300

    Rollback index

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,11 +6,13 @@ const User = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Please enter a full name'],
+      index: true,
     },
     email: {
       type: String,
       lowercase: true,
       unique: true,
+      index: true,
     },
     password: String,
     salt: String,
