a2514a43800d9514cbaa3caa8db2f01d27c3ec26
burkaydurdu <burkaydurdu@outlook.com>
2021-03-24 12:10:12 +0300

    Remove index for special db

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,13 +6,11 @@ const User = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Please enter a full name'],
-      index: true,
     },
     email: {
       type: String,
       lowercase: true,
       unique: true,
-      index: true,
     },
     password: String,
     salt: String,
